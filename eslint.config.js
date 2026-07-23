39438137ce07fa772cbe4c1489a229b703666ac0
salmaan-25 <mohamed.salmaan2004@gmail.com>
2026-07-23 22:57:10 +0530

    chore: configure typescript eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
-export default [
+export default tseslint.config(
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
-    ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**'],
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.turbo/**', '**/storybook-static/**'],
   },
   {
     rules: {
-      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
   },
-]
+)
